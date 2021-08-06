829c5f2247bc765faba681bff78803ab3d2439c3
Gui-dev <adrianon10@gmail.com>
2021-08-06 01:51:26 -0300

    first commit

---
+++ index.ts
@@ -0,0 +1,8 @@
+import { App } from './src/shared/infra/http/App'
+
+const app = new App().server
+const PORT = process.env.PORT || 3333
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`)
+})
