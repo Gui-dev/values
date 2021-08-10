c04cb7f1554862527c5a735a068899870be6f225
Gui-dev <adrianon10@gmail.com>
2021-08-10 03:28:33 -0300

    refactor: add changed name

--- src/modules/user/infra/typeorm/repositories/UserRepository.ts
+++
@@ -1,8 +0,0 @@
-import { EntityRepository, Repository } from 'typeorm'
-
-import { User } from '@modules/user/infra/typeorm/entities/User'
-
-@EntityRepository(User)
-export class UserRepository extends Repository<User> {
-
-}
