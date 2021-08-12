3e83886f175a37d0b24060f85dbc2a8082654840
Gui-dev <adrianon10@gmail.com>
2021-08-12 03:12:21 -0300

    style: add single quotes

--- src/modules/user/infra/http/controllers/UsersController.ts
+++ src/modules/user/infra/http/controllers/UsersController.ts
@@ -1,4 +1,4 @@
-import { Request, Response } from "express";
+import { Request, Response } from 'express';
 
 import { CreateUsersService } from '@modules/user/infra/typeorm/services/CreateUsersService'
 
