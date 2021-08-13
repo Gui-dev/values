5616291766c071ad54007495777dd90639cb8467
Gui-dev <adrianon10@gmail.com>
2021-08-13 20:18:42 -0300

    feat: add ComplimentsController controller

---
+++ src/modules/compliments/infra/http/controllers/ComplimentsController.ts
@@ -0,0 +1,19 @@
+import { Request, Response } from 'express'
+
+import { CreateComplimentsService } from '@modules/compliments/services/CreateComplimentsService'
+
+export class ComplimentsController {
+  public async create (request: Request, response: Response): Promise<Response> {
+    const { user_sender, user_receiver, tag_id, message } = request.body
+    const createComplimentsService = new CreateComplimentsService()
+
+    const compliment = await createComplimentsService.execute({
+      user_sender,
+      user_receiver,
+      tag_id,
+      message
+    })
+
+    return response.status(201).json(compliment)
+  }
+}
