a069e1efd45ae199cb6ec039bbc5ee2c43cf62eb
Gui-dev <adrianon10@gmail.com>
2021-08-13 02:48:20 -0300

    feat: add new route /login

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,10 +1,14 @@
 import { Router } from 'express'
+
 import { usersRoutes } from '@modules/user/infra/http/routes/users.routes'
 import { tagsRoutes } from '@modules/tag/infra/http/routes/tags.routes'
+import { sessionRoutes } from '@modules/user/infra/http/routes/session.routes'
 
 const routes = Router()
 
 routes.use('/users', usersRoutes)
+routes.use('/login', sessionRoutes)
+
 routes.use('/tags', tagsRoutes)
 
 export default routes
