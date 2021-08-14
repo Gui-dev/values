073b65bc012e93f0fd0e05a290d36a897cc4ac8d
Gui-dev <adrianon10@gmail.com>
2021-08-14 04:11:20 -0300

    feat: add looking for user

--- src/modules/user/infra/http/middlewares/ensureAdmin.ts
+++ src/modules/user/infra/http/middlewares/ensureAdmin.ts
@@ -1,14 +1,17 @@
-import { NextFunction, Request, Response } from "express";
+import { NextFunction, Request, Response } from 'express'
+import { getCustomRepository } from 'typeorm'
 
+import { UsersRepository } from '../../typeorm/repositories/UsersRepository'
+import { AppError } from '@shared/error/AppError'
 
 export const ensureAdmin = async (request: Request, response: Response, next: NextFunction): Promise<Response | void> => {
-  const admin = true
+  const id = request.userId
+  const usersRepository = getCustomRepository(UsersRepository)
+  const { admin } = await usersRepository.findOne(id)
 
   if (admin) {
     return next()
   }
 
-  return response.status(401).json({
-    error: 'Unauthorized'
-  })
+  throw new AppError('Unauthorized', 401)
 }
