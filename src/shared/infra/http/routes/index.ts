413bbaa61beb7ef1a55767b434c91e0e0ff5b011
Gui-dev <adrianon10@gmail.com>
2021-08-14 04:12:00 -0300

    feat: add ensureAuthentication

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -5,11 +5,14 @@ import { tagsRoutes } from '@modules/tag/infra/http/routes/tags.routes'
 import { sessionRoutes } from '@modules/user/infra/http/routes/session.routes'
 import { complimentsRoutes } from '@modules/compliments/infra/http/routes/compliments.routes'
 
+import { ensureAuthentication } from '@modules/user/infra/http/middlewares/ensureAuthentication'
+
 const routes = Router()
 
 routes.use('/users', usersRoutes)
 routes.use('/login', sessionRoutes)
 
+routes.use(ensureAuthentication)
 routes.use('/tags', tagsRoutes)
 routes.use('/compliments', complimentsRoutes)
 
