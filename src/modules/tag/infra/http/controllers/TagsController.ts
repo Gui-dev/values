e0f45c37f40dab3b55019b656c5c96f44a6d04c4
Gui-dev <adrianon10@gmail.com>
2021-08-17 04:12:17 -0300

    feat: add helper method index

--- src/modules/tag/infra/http/controllers/TagsController.ts
+++ src/modules/tag/infra/http/controllers/TagsController.ts
@@ -1,8 +1,15 @@
 import { Request, Response } from 'express'
+import { classToClass } from 'class-transformer'
 
 import { TagsService } from '@modules/tag/services/TagsService'
 
 export class TagsController {
+  public async index (request: Request, response: Response): Promise<Response> {
+    const tagsService = new TagsService()
+    const tags = await tagsService.listTags()
+
+    return response.status(201).json(classToClass(tags))
+  }
   public async create (request: Request, response: Response): Promise<Response> {
     const { name } = request.body
     const tagsService = new TagsService()
