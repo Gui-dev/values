eab234e42dee2ee9878053b8df9a0b31f1d060b3
Gui-dev <adrianon10@gmail.com>
2021-08-17 03:53:49 -0300

    feat: add helper methods showUserSendCompliments and showUserReceiverCompliments

--- src/modules/compliments/infra/http/controllers/ComplimentsController.ts
+++ src/modules/compliments/infra/http/controllers/ComplimentsController.ts
@@ -1,20 +1,37 @@
 import { Request, Response } from 'express'
+import { classToClass } from 'class-transformer'
 
-import { CreateComplimentsService } from '@modules/compliments/services/CreateComplimentsService'
+import { ComplimentsService } from '@modules/compliments/services/ComplimentsService'
 
 export class ComplimentsController {
   public async create (request: Request, response: Response): Promise<Response> {
     const { user_receiver, tag_id, message } = request.body
     const user_sender = request.userId
-    const createComplimentsService = new CreateComplimentsService()
+    const complimentsService = new ComplimentsService()
 
-    const compliment = await createComplimentsService.execute({
+    const compliment = await complimentsService.execute({
       user_sender,
       user_receiver,
       tag_id,
       message
     })
 
-    return response.status(201).json(compliment)
+    return response.status(201).json(classToClass(compliment))
+  }
+
+  public async showUserSendCompliments (request: Request, response: Response): Promise<Response> {
+    const user_sender = request.userId
+    const complimentsService = new ComplimentsService()
+    const compliments = await complimentsService.listUserSendCompliments(user_sender)
+
+    return response.status(201).json(classToClass(compliments))
+  }
+
+  public async showUserReceiverCompliments (request: Request, response: Response): Promise<Response> {
+    const user_receiver = request.userId
+    const complimentsService = new ComplimentsService()
+    const compliments = await complimentsService.listUserReceiverCompliments(user_receiver)
+
+    return response.status(201).json(classToClass(compliments))
   }
 }
