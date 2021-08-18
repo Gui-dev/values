6f4c7b40176bae8b06cb364539204caf0930a146
Gui-dev <adrianon10@gmail.com>
2021-08-18 02:41:57 -0300

    feat: add helper method index

--- src/modules/user/infra/http/controllers/UsersController.ts
+++ src/modules/user/infra/http/controllers/UsersController.ts
@@ -1,13 +1,20 @@
 import { Request, Response } from 'express'
 import { classToClass } from 'class-transformer'
 
-import { CreateUsersService } from '@modules/user/services/CreateUsersService'
+import { UsersService } from '@modules/user/services/UsersService'
 
 export class UsersController {
+  public async index (request: Request, response: Response): Promise<Response> {
+    const usersService = new UsersService()
+    const users = await usersService.listUsers()
+
+    return response.status(201).json(classToClass(users))
+  }
+
   public async create (request: Request, response: Response): Promise<Response> {
     const { name, email, password, admin } = request.body
-    const createUsersService = new CreateUsersService()
-    const user = await createUsersService.execute({ name, email, password, admin })
+    const usersService = new UsersService()
+    const user = await usersService.execute({ name, email, password, admin })
 
     return response.status(201).json(classToClass(user))
   }
