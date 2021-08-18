d0a48ec49bb41aaa00c9daaa01c0672791df7255
Gui-dev <adrianon10@gmail.com>
2021-08-18 02:41:24 -0300

    feat: add new route

--- src/modules/user/infra/http/routes/users.routes.ts
+++ src/modules/user/infra/http/routes/users.routes.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express'
 import { UsersController } from '@modules/user/infra/http/controllers/UsersController'
+import { ensureAuthentication } from '@modules/user/infra/http/middlewares/ensureAuthentication'
 
 const usersRoutes = Router()
 const usersController = new UsersController()
 
 
+usersRoutes.get('/', ensureAuthentication, usersController.index)
 usersRoutes.post('/', usersController.create)
 
 export { usersRoutes }
